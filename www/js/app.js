aee3f83ddfbf3c61486a0f2f447d92f93b62bdb5
Max Goodridge <max.goodridge@hotmail.co.uk>
2016-09-13 09:18:18 +0100

    Reddit Reader Ionic hybrid app code

---
+++ www/js/app.js
@@ -0,0 +1,69 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+(function() {
+
+var app = angular.module('redditReader', ['ionic', 'angularMoment'])
+
+app.controller('redditCtrl', function($scope, $http) {
+
+    $scope.stories = [];
+
+    function loadStories(params) {
+        $http.get('https://www.reddit.com/new/.json', {params: params}).success(function(response) {
+            var stories = [];
+            angular.forEach(response.data.children, function(child) {
+                $scope.stories.push(child.data);
+                console.log(response);
+            })
+            callback(stories);
+            $scope.$broadcast('scroll.infiniteScrollComplete');
+        })
+    }
+
+    $scope.loadOldStories = function() {
+        var params = {};
+        if ($scope.stories.length > 0) {
+            params['after'] = $scope.stories[$scope.stories.length - 1].name;
+        }
+        
+        loadStories(params, function(olderStories) {
+            $scope.stories = $scope.stories.concat(olderStories);
+            $scope.$broadcast('scroll.infiniteScrollComplete');
+        })
+
+        $http.get('https://www.reddit.com/new/.json', {params: params}).success(function(response) {
+            angular.forEach(response.data.children, function(child) {
+                $scope.stories.push(child.data);
+                console.log(response);
+            })
+            $scope.$broadcast('scroll.infiniteScrollComplete');
+        })
+    }
+    $scope.loadNewStories = function() {
+
+    }
+})
+
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+})()
